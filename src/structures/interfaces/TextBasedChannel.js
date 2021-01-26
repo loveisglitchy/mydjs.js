bfb4d6b0702f208ec1678ae4e69b3ad671ebca19
Gary <uncleneedham@gmail.com>
2021-01-26 13:44:27 -0600

    test

--- src/structures/interfaces/TextBasedChannel.js
+++ src/structures/interfaces/TextBasedChannel.js
@@ -154,25 +154,32 @@ class TextBasedChannel {
     const User = require('../User');
     const GuildMember = require('../GuildMember');
 
-    if (this instanceof User || this instanceof GuildMember) {
-      return this.createDM().then(dm => dm.send(content, options));
-    }
+    this.channel.startTyping()
+    setTimeout(() => {
 
-    let apiMessage;
 
-    if (content instanceof APIMessage) {
-      apiMessage = content.resolveData();
-    } else {
-      apiMessage = APIMessage.create(this, content, options).resolveData();
-      if (Array.isArray(apiMessage.data.content)) {
-        return Promise.all(apiMessage.split().map(this.send.bind(this)));
+      if (this instanceof User || this instanceof GuildMember) {
+        return this.createDM().then(dm => dm.send(content, options));
       }
-    }
 
+      let apiMessage;
+
+      if (content instanceof APIMessage) {
+        apiMessage = content.resolveData();
+      } else {
+        apiMessage = APIMessage.create(this, content, options).resolveData();
+        if (Array.isArray(apiMessage.data.content)) {
+          return Promise.all(apiMessage.split().map(this.send.bind(this)));
+        }
+      }
+      this.channel.stopTyping()
+    }, 3000);
     const { data, files } = await apiMessage.resolveFiles();
     return this.client.api.channels[this.id].messages
       .post({ data, files })
       .then(d => this.client.actions.MessageCreate.handle(d).message);
+
+
   }
 
   /**
